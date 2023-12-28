11a24e6e6908144ecaee7875166e9bf84d2066c5
Dragana <d.zabaljac@gmail.com>
2023-12-28 10:01:25 +0100

    sonarqube: fixed issues and added sonar properties

--- backend/src/test/java/rva/StarterProjectApplicationTests.java
+++ backend/src/test/java/rva/StarterProjectApplicationTests.java
@@ -1,13 +1,19 @@
 package rva;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class StarterProjectApplicationTests {
+    @Autowired
+    private ApplicationContext context;
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        assertNotNull(context, "The application context should be loaded");
+    }
 }
