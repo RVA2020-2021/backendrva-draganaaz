2e7650116abd8ddd81bda03c3fd0004772c5eee6
Dragana <d.zabaljac@gmail.com>
2023-12-29 12:19:53 +0100

    SQ: deleted pocetna css as it was empty

--- frontend/src/app/components/pocetna/pocetna.component.ts
+++ frontend/src/app/components/pocetna/pocetna.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pocetna',
   templateUrl: './pocetna.component.html',
-  styleUrls: ['./pocetna.component.css']
 })
 export class PocetnaComponent implements OnInit {
 
